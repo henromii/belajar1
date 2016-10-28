542f1e586e895154135b73dadb3710c42bc64279
Henromi <Henromi@Henromi-PC>
2016-10-28 15:09:30 +0700

    Mohamad Henromi

--- config/koneksi.php
+++ config/koneksi.php
@@ -5,3 +5,14 @@
  *belajar pertama pemprograman php  
  */
 
+$server="localhost";
+$user="root";
+$password="";
+$database="lesson";
+$koneksi= mysqli_connect($server, $user, $password, $database);
+
+if($koneksi){
+   echo"berhasil terkoneksi";
+}else{ 
+    echo "gagal ke koneksi";
+}
